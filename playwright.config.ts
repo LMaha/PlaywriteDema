b2750696c008fe8acee6f8049cb143592f7c3490
LMaha <41241959+LMaha@users.noreply.github.com>
2026-05-29 14:54:45 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+import path from 'path';
 import { defineConfig } from '@playwright/test';
+
+// Read from default .env file
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+
 
 export default defineConfig({
   testDir: './tests',
